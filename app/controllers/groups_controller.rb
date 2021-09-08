51253fe16a877479269bc5c557681a99a8bf4c13
304089 <9457imai2@gmail.com>
2021-09-08 14:12:56 +0000

    ユーザー登録、編集、一覧作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,37 @@
 class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.new(group_params)
+    group.owner_id = current_user.id
+    group.save
+    group_user = GroupUser.create(user_id: current_user.id,group_id: group.id)
+    redirect_to groups_path
+  end
+
+  def show
+  end
+
+  def index
+    @groups = Group.all
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+    group.update(group_params)
+    redirect_to groups_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name,:introduction,:image,:owner)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_124455) do
+ActiveRecord::Schema.define(version: 2021_09_08_140353) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_09_08_124455) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
-    t.string "introductio"
+    t.string "introduction"
     t.string "image_id"
     t.string "owner_id"
     t.datetime "created_at", null: false
