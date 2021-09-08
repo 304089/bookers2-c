8d71822df6311f2d61b2c49fa12c57467a58d0cd
304089 <9457imai2@gmail.com>
2021-09-08 12:38:52 +0000

    応用課題c開始

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  devise_for :users
+  get '/home/about' => 'homes#about'
+  get '/search' => 'searches#search'
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+
+  resources :books do
+    resources :book_comments,only: [:create,:destroy]
+    resource :favorites,only: [:create,:destroy]
+  end
+
+end
